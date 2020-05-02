1aa7202261c00b92157fab52707deb601bf78df8
Cuan <cuanmeyer@gmail.com>
2020-05-02 21:54:03 +0200

    another commit

--- lib/global.dart
+++ lib/global.dart
@@ -2,7 +2,6 @@ import 'package:http/http.dart';
 import 'package:corona_south_africa_v1/services/network_call.dart';
 
 final client = Client();
-
 final netWorkCalls = NetworkCall();
 
 final RegExp reg = new RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))');
